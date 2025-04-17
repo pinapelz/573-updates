301a47f11ac8dd93d722c75ae606ea2d123e8a63
Pinapelz <yukais@pinapelz.com>
2025-04-17 00:07:40 -0700

    fix: merge formatting issue
    thanks a lot git

--- site/src/utils.ts
+++ site/src/utils.ts
@@ -10,11 +10,7 @@ export const getGameTitle = (gameId: string) => {
     if (lowerCaseGameId.startsWith("maimaidx_intl")) return "maimai DX (INTERNATIONAL)";
     if (lowerCaseGameId.startsWith("ongeki_jp")) return "O.N.G.E.K.I"
     if (lowerCaseGameId.startsWith("chunithm_intl")) return "CHUNITHM (INTERNATIONAL)"
-<<<<<<< Updated upstream
-    if (lowerCaseGameId.startsWIth("ddr")) return "DanceDanceRevolution"
-=======
     if (lowerCaseGameId.startsWith("ddr")) return "DanceDanceRevolution"
->>>>>>> Stashed changes
 
     return gameId.toUpperCase();
 };
