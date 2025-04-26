4852c740b0e967429f61228511af18ea25a77c12
Pinapelz <yukais@pinapelz.com>
2025-04-25 22:04:30 -0700

    vercel analytics

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
